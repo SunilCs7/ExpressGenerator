2d4282cea71db7a380b915863f217aeb84919ea6
Sunil Prasad Yadav <sunilpro137@gmail.com>
2024-01-02 13:37:05 +0530

    database setup and creatation

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+// step-1: install mangodb
+// step-2: install mangoose->inp i mangoose
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// step-3:Require and setup connection
+const mongoose = require("mongoose"); //require mongoose
+
+mongoose.connect("mongodb://127.0.0.1:27017/Practicekarojaaneman"); // setup database in database with base prace..
+
+// step-4: make schema
+const userSchema = mongoose.Schema({
+  //create a new instance of the Schema object of user
+  username: String,
+  name: String,
+  email: String,
+  age: Number,
 });
 
-module.exports = router;
+// step-5:create model and export
+
+mangoose.model("user", userSchema);
